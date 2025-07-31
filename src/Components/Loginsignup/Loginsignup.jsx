bc222900ec82e322316b9cbb5c988e3c3dbd8142
kayaththiri-kanagasabai <kaya282002@gmail.com>
2025-07-31 21:32:15 +0530

    finished page

--- src/Components/Loginsignup/Loginsignup.jsx
+++ src/Components/Loginsignup/Loginsignup.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Loginsignup.css';
 import email from '../Assets/email.png';
 import password from '../Assets/password.png';
 import person from '../Assets/person.png'
 
 const Loginsignup = () => {
+  const[action,setAction]=useState("Sign Up");
   return (
     <div className='container'>
      <div className="header">
-        <div className="text">Sign Up</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
      </div>
      <div className="inputs">
-        <div className="input">
+      {action==='Login'?<div></div>:<div className="input">
             <img src={person} alt="" />
-            <input type="text" />
-        </div>
+            <input type="text" placeholder='Name' />
+        </div>}
+        
+        
         <div className="input">
             <img src={email} alt="" />
-            <input type="email" />
+            <input type="email" placeholder='Email Id'/>
         </div>
         <div className="input">
             <img src={password} alt="" />
-            <input type="password" />
+            <input type="password" placeholder='Password' />
         </div>
      </div>
-     <div className="forgot-password">Lost Password? <span>Click Here..</span></div>
+     {action==='Sign Up'?<div></div>: <div className="forgot-password">Lost Password? <span>Click Here..</span></div>}
+    
      <div className="submit-container">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Login</div>
+        <div className={action==='Login'?'submit gray':"submit"} onClick={()=>{setAction("Sign Up")}} >Sign Up</div>
+        <div className={action==='Sign Up'? 'submit gray':"submit" } onClick={()=>{setAction("Login")}}>Login</div>
      </div>
     </div>
   )
